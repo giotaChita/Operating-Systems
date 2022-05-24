85153bf602d9958260a45729cd0cd46a3419ddd7
giotaChita <69098448+giotaChita@users.noreply.github.com>
2022-05-24 20:20:33 +0200

    Update DiningP_mutex_th.c

--- task7/DiningP_mutex_th.c
+++ task7/DiningP_mutex_th.c
@@ -1,97 +1,104 @@
+/* https://stackoverflow.com/questions/69578246/c-dining-philosophers-how-to-make-concurrent-threads-wait-till-a-condition-i */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-#include <semaphore.h>
+
+#define N	5
+#define LEFT	( i + N - 1 ) % N
+#define RIGHT	( i + 1 ) % N
 
 #define THINKING 0
 #define HUNGRY 1
 #define EATING 2
-#define PHILOSOPHERS 5
 
+pthread_mutex_t m;
+int state[N];
+pthread_mutex_t s[N];
+int eat_count[N];
+int running = 1;
+void test(int i){
+    if(state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING){
+        state[i] = EATING;
+        eat_count[i]++;
+        pthread_mutex_unlock(&s[i]);
+    }
+}
 
-#define LEFT (i + PHILOSOPHERS - 1) % PHILOSOPHERS
-#define RIGHT (i + 1) % PHILOSOPHERS
+void grab_forks(int i){
+    pthread_mutex_lock(&m);
+    state[i] = HUNGRY;
+    test(i);
+    pthread_mutex_unlock(&m);
+    pthread_mutex_lock(&s[i]);
+}
 
-sem_t sem[PHILOSOPHERS];
-pthread_mutex_t mutex;
+void put_away_forks(int i){
+    pthread_mutex_lock(&m);
+    state[i] = THINKING;
+    printf("Philosopher[%d] finishes eating...\n", i);
+    test(LEFT);
+    test(RIGHT);
+    pthread_mutex_unlock(&m);
+}
 
-int state[PHILOSOPHERS];
 
 void think(int i){
     printf("Philosopher[%i] is thinking...\n", i);
-    sleep(rand() %10);
+    sleep(rand() %5+1);
 }
 
 void eat(int i){
     printf("Philosopher[%i] is eating...\n",i);
-    sleep(rand()%10);
+    sleep(rand()%5+1);
 }
 
-void test(int i){
-    if(state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING){
-        state[i] = EATING;
-        sem_post(&sem[i]); // unlocks the semaphore pointed to by sem.
-    }
-}
-
-void grap_forks(int i){
-    pthread_mutex_lock(&mutex);
-    state[i] = HUNGRY;
-    test(i);
-    pthread_mutex_unlock(&mutex);
-    sem_wait(&sem[i]); // locks the semaphore pointed to by sem.
-}
-
-void put_away_forks(int i){
-    pthread_mutex_lock(&mutex);
-    state[i] = THINKING;
-    test(LEFT);
-    test(RIGHT);
-    pthread_mutex_unlock(&mutex);
-}
-
-
-void *philosopher(void *arg){
-    int i = *(int *)arg;
+void* philosopher(void* arg){
+    int id =*(int *)arg;
     free(arg);
-    while(1){
-        think(i);
-        grap_forks(i);
-        eat(i);
-        printf("Philosopher[%i] done eating...\n", i);
-        printf("==============================\n");
-
-        put_away_forks(i);
-
+    printf("Philosopher[%d] join the table.\n", id);
+    while(running){
+        think(id);
+        grab_forks(id);
+        eat(id);
+        put_away_forks(id);
+        printf("=============================\n");
     }
+}
+
+void* runtime(){
+   sleep(40); 
+   running=0;
 }
 
 int main(void){
-    int i;
-    int j;
+    pthread_t p_id[N],total_time;
+    pthread_mutex_init(&m,NULL);
     int *arg;
-    pthread_t tid[PHILOSOPHERS];
-
-    for(j = 0; j < PHILOSOPHERS; j++){
-        sem_init(&sem[j] , 0, 0);
+    for(int i = 0; i<N;i++){
+        pthread_mutex_init(s+1,NULL);
+        pthread_mutex_lock(&s[i]);
     }
+    pthread_create(&total_time,NULL,&runtime,NULL);
 
-    // create philosopher thread
-    for(i = 0; i< PHILOSOPHERS; i++){
+    for(int i = 0; i< N; i++){
         arg = (int *)malloc(sizeof(int));
         *arg = i;
-        pthread_create(&tid[i], NULL, philosopher, arg);
+        pthread_create(&p_id[i], NULL, &philosopher, arg);
     }
 
-    // wait to finish the dinner
-    for( i = 0; i < PHILOSOPHERS; i++){
-        pthread_join(tid[i], NULL);
+    for(int i = 0; i < N; i++){
+        pthread_join(p_id[i], NULL);
     }
 
-    for(j = 0; j < PHILOSOPHERS; j++){
-        sem_destroy(&sem[j]);
+    pthread_mutex_destroy(&m);
+    for(int i=0;i<N;i++){
+        pthread_mutex_destroy( &s[i] );
     }
-    return EXIT_SUCCESS;
+    printf("End of dinner\n");
+    for(int i=0;i<N;i++){
+      printf("Philosopher [%d] ate %d times\n",i,eat_count[i]);
+    }
+    return 0;
 }
-
